ec6ead43db9b0ea210aa8a6d8924e4f051d5b4cc
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2017-09-28 18:01:52 +0400

    Header navigation

--- basic/app/scripts/app.js
+++ basic/app/scripts/app.js
@@ -1,4 +1,18 @@
 jQuery(document).ready(function($){
+	
+
+	/* SET EQUAL HEIGHT FOR SUBMENU'S BACK */
+	if ( $('header[role="banner"] .dropdown').length > 0 )
+	{
+		var dd_h = 0;
+
+		$('header[role="banner"] .dropdown').each(function(){
+			if ( $(this).outerHeight(true) > dd_h )
+				dd_h = $(this).outerHeight(true);
+		});
+
+		$('header[role="banner"] .dropdown .dropdown_back').height(dd_h);
+	}
 
 
 });
--- basic/dist/assets/js/pp.js
+++ basic/dist/assets/js/pp.js
@@ -560,6 +560,20 @@ References:
 	};
 })(this);
 jQuery(document).ready(function($){
+	
+
+	/* SET EQUAL HEIGHT FOR SUBMENU'S BACK */
+	if ( $('header[role="banner"] .dropdown').length > 0 )
+	{
+		var dd_h = 0;
+
+		$('header[role="banner"] .dropdown').each(function(){
+			if ( $(this).outerHeight(true) > dd_h )
+				dd_h = $(this).outerHeight(true);
+		});
+
+		$('header[role="banner"] .dropdown .dropdown_back').height(dd_h);
+	}
 
 
 });
