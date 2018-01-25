9ed333298e5f5c4f4f91af1ff01fb3bb89a3549a
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2018-01-26 00:23:55 +0400

    Стрелки в продукте

--- basic/app/scripts/plugins/accordion.js
+++ basic/app/scripts/plugins/accordion.js
@@ -1,5 +1,19 @@
 $(".js-accordion--tab").on('click', function(e){
   e.preventDefault();
   e.stopPropagation();
-  $(this).next('.js-accordion--tabContent').toggle(400);
+
+	var $this = $(this);
+	var c = 'opened';
+
+	if( $this.hasClass(c) )
+		$this.removeClass(c);
+	else
+		$this.addClass(c);
+
+	var $tab = $this.next('.js-accordion--tabContent');
+
+  	if( $tab.hasClass(c) )
+		$tab.removeClass(c);
+	else
+		$tab.addClass(c);
 });
--- basic/dist/assets/js/pp.js
+++ basic/dist/assets/js/pp.js
@@ -1,7 +1,21 @@
 $(".js-accordion--tab").on('click', function(e){
   e.preventDefault();
   e.stopPropagation();
-  $(this).next('.js-accordion--tabContent').toggle(400);
+
+	var $this = $(this);
+	var c = 'opened';
+
+	if( $this.hasClass(c) )
+		$this.removeClass(c);
+	else
+		$this.addClass(c);
+
+	var $tab = $this.next('.js-accordion--tabContent');
+
+  	if( $tab.hasClass(c) )
+		$tab.removeClass(c);
+	else
+		$tab.addClass(c);
 });
 /**
  * AnySlide v 1.0
